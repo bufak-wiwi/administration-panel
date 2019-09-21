2c91e3b8a4d396921085488e92bba8a2743d03e9
Lukas Laptop <lukasc@mail.upb.de>
2019-09-21 15:00:26 +0200

    feat(): New Missing Permissions Image

--- src/components/MissinPermissions.js
+++ src/components/MissinPermissions.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import Typography from './Typography'
-import bn from 'utils/bemnames';
-const bem = bn.create('page');
+import { Row } from 'reactstrap'
+import Emptylayout from './Layout/EmptyLayout'
+const missingPermissions = require('../assets/img/octocat/missingPermissions.png');
+
 const MissingPermissions = ( ) => {
   return (
-    <div className="cr-page-spinner">
-            <Typography type="h3" className={bem.e('title')}>
-             Keine Berechtigungen
-          </Typography>
-    </div>
+    <Emptylayout>
+      <Row style={{justifyContent: 'center'}}>
+       <img src={missingPermissions} alt="OCTOCAT!"/>
+      </Row>
+    </Emptylayout>
   );
 };
 
