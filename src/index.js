d1e3799c99339b93cde816c306d281ac35a0eb90
Lukas Laptop <lukasc@mail.upb.de>
2020-01-28 21:22:05 +0100

    feat(): register service worker

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import { Provider } from 'react-redux'
 import App from './App';
 
 import store from './redux/store';
+import * as serviceWorker from './registerServiceWorker';
 
 ReactDOM.render(
         <Provider store={store}>
             <App />
         </Provider>,
 document.getElementById('root'));
+
+serviceWorker.register();
