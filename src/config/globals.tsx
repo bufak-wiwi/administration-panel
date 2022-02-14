005c5d0d4ea928bc41c5a44553c7cc4f22e70333
Leonard Heimann <46447465+l-heimann@users.noreply.github.com>
2022-02-14 16:13:57 +0100

    Update globals.tsx

--- src/config/globals.tsx
+++ src/config/globals.tsx
@@ -1,3 +1,3 @@
 export const conferenceID = 2;
-export const baseURL: string = "https://localhost:5001/api"
+export const baseURL: string = "https://dev.api.bufak-wiso.de:8443/api"
 export const apiKey: string = "testapikey"
