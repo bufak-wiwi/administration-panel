b94b5ccfd91379ae22de632363c26ae124535497
Leonard Heimann <46447465+l-heimann@users.noreply.github.com>
2023-01-12 16:45:02 +0100

    BuFaK Leipzig 2023

--- src/components/Widget/UploadInformationCard.js
+++ src/components/Widget/UploadInformationCard.js
@@ -20,10 +20,10 @@ class UploadInformationCard extends Component {
                 <Alert color="grey">
                     Liebe Teilnehmende,<br />
                     willkommen auf unserem Konfetti-Anmeldetool!<br />
-                    Hier könnt Ihr Euch mit Eurer Prioritätenliste bis zum 30.09.2022 um 20 Uhr anmelden.<br />
-                    Da die Konferenz vom BMBF unterstützt wird, benötigen wir von jeder angemeldeten Person eine aktuelle Immatrikulationsbescheinigung. Bitte benennt die PDF-Datei folgendermaßen: "Stadt_Vorname_Nachname". <br />
-                    Diese könnt Ihr unter folgendem Link hochladen: <a href="https://glidser.bufak-wiwi.org/s/Pqqic53P4GmpcSg" target="_blank" rel="noopener noreferrer">https://glidser.bufak-wiwi.org/s/Pqqic53P4GmpcSg</a> <br />
-                    Bei Fragen und Anregungen meldet Euch gerne unter dieser E-Mail bei uns: <a href="mailto:teilnehmende.bufak@uni-oldenburg.de" target="_blank" rel="noopener noreferrer">teilnehmende.bufak@uni-oldenburg.de</a>
+                    Hier könnt Ihr Euch mit Eurer Prioritätenliste für die 76. Bundesfachschaftenkonferenz Wirtschaftswissenschaften vom 23.01.2023 bis zum 17.02.2023 EOD anmelden.<br />
+                    Da die Konferenz vom BMBF unterstützt wird, benötigen wir von jeder angemeldeten Person eine aktuelle Immatrikulationsbescheinigung. Bitte benennt die PDF-Datei folgendermaßen: "Stadt_Vorname_Nachname".<br />
+                    Diese könnt Ihr unter folgendem Link hochladen: <a href="https://glidser.bufak-wiwi.org/s/bHDxFMYDJgGdeYi" target="_blank" rel="noopener noreferrer">https://glidser.bufak-wiwi.org/s/bHDxFMYDJgGdeYi</a><br />
+                    Bei Fragen und Anregungen meldet Euch gerne unter dieser E-Mail bei uns: <a href="mailto:bufakteilnehmende@fsrwiwi-leipzig.de" target="_blank" rel="noopener noreferrer">bufakteilnehmende@fsrwiwi-leipzig.de</a>
                 </Alert>
             </CardBody> 
         </Card>
