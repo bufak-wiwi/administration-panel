a2f8d30eb83473872a867d08eeed9a13d8dfdb07
zacdet <31074759+zacdet@users.noreply.github.com>
2025-01-05 22:00:59 +0100

    Update UploadInformationCard.js
    Update Infotext for BuFaK 80

--- src/components/Widget/UploadInformationCard.js
+++ src/components/Widget/UploadInformationCard.js
@@ -15,16 +15,16 @@ class UploadInformationCard extends Component {
 
     return (
         <Card>
-            <CardHeader >Studienbescheinigung hochladen</CardHeader>
+            <CardHeader>Studienbescheinigung hochladen</CardHeader>
             <CardBody>
                 <Alert color="grey">
                     Herzlich Willkommen auf unserem Konfetti-Anmeldetool!<br /><br />
-                    Hier könnt Ihr Euch mit Eurer Prioritätenliste für die 79. Bundesfachschaftenkonferenz Wirtschaftswissenschaften vom 01.08.2024 bis zum 30.08.2024 EOD anmelden.<br /><br />
+                    Hier könnt Ihr Euch mit Eurem Prioritätscode für die 80. Bundesfachschaftenkonferenz Wirtschaftswissenschaften vom 12.01.2025 bis zum 12.02.2025 EOD anmelden.<br /><br />
                     Da die Konferenz vom BMBF unterstützt wird, benötigen wir von jeder angemeldeten Person eine aktuelle Immatrikulationsbescheinigung. Bitte benennt die PDF-Datei folgendermaßen: "Stadt_Vorname_Nachname".<br />
-                    Diese könnt Ihr unter folgendem Link hochladen: <a href="https://uni-wuppertal.sciebo.de/s/YONzJykwa68SoLv" target="_blank" rel="noopener noreferrer">https://uni-wuppertal.sciebo.de/s/YONzJykwa68SoLv</a><br /><br />
-                    Bei Fragen und Anregungen meldet Euch gerne unter dieser E-Mail bei uns: <a href="mailto:teilnehmende-bufak@wiwi.uni-wuppertal.de" target="_blank" rel="noopener noreferrer">teilnehmende-bufak@wiwi.uni-wuppertal.de</a>
+                    Diese könnt Ihr unter folgendem Link hochladen: <a href="https://www.dropbox.com/request/KXJGATgNINqOT7vwG2UD" target="_blank" rel="noopener noreferrer">https://www.dropbox.com/request/KXJGATgNINqOT7vwG2UD</a><br /><br />
+                    Bei Fragen und Anregungen meldet Euch gerne unter dieser E-Mail bei uns: <a href="mailto:teilnehmende.bufak-wiwi@uni-bayreuth.de" target="_blank" rel="noopener noreferrer">teilnehmende.bufak-wiwi@uni-bayreuth.de</a>
                 </Alert>
-            </CardBody> 
+            </CardBody>
         </Card>
     )
   }
