1de665c4d6c0ab443fe8d9587b031ce8cddaa547
zacdet <31074759+zacdet@users.noreply.github.com>
2026-01-18 22:50:54 +0100

    Update UploadInformationCard.js

--- src/components/Widget/UploadInformationCard.js
+++ src/components/Widget/UploadInformationCard.js
@@ -21,7 +21,7 @@ class UploadInformationCard extends Component {
                     Herzlich Willkommen auf unserem Konfetti-Anmeldetool!<br /><br />
                     Hier könnt Ihr Euch mit Eurem Prioritätscode für die 82. Bundesfachschaftenkonferenz Wirtschaftswissenschaften vom 18.01.2026 bis zum 22.02.2026 EOD anmelden.<br /><br />
                     Da die Konferenz vom BMBF unterstützt wird, benötigen wir von jeder angemeldeten Person eine aktuelle Immatrikulationsbescheinigung. Bitte benennt die PDF-Datei folgendermaßen: "Stadt_Vorname_Nachname".<br />
-                    Diese könnt Ihr unter folgendem Link hochladen: <a href="https://1drv.ms/b/c/8541847e6e62c9f1/IQBIwYgxHAqUS7jfboInn_K6AdMo_oAJnZmgSck9pEDEtek?e=pD8XLK " target="_blank" rel="noopener noreferrer">https://1drv.ms/b/c/8541847e6e62c9f1/IQBIwYgxHAqUS7jfboInn_K6AdMo_oAJnZmgSck9pEDEtek?e=pD8XLK </a><br /><br />
+                    Diese könnt Ihr unter folgendem Link hochladen: <a href="https://www.dropbox.com/request/lutk7zJgHnlOtlLFux3y" target="_blank" rel="noopener noreferrer">https://www.dropbox.com/request/lutk7zJgHnlOtlLFux3y</a><br /><br />
                     Bei Fragen und Anregungen meldet Euch gerne unter dieser E-Mail bei uns: <a href="mailto:guest@stugawiwi.de" target="_blank" rel="noopener noreferrer">guest@stugawiwi.de</a>
                 </Alert>
             </CardBody>
