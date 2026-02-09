853e24e1173d2049ab77ea781678cfff3596b470
zacdet <31074759+zacdet@users.noreply.github.com>
2026-02-09 14:32:16 +0100

    Update UploadInformationCard.js

--- src/components/Widget/UploadInformationCard.js
+++ src/components/Widget/UploadInformationCard.js
@@ -21,7 +21,7 @@ class UploadInformationCard extends Component {
                     Herzlich Willkommen auf unserem Konfetti-Anmeldetool! <br /><br />
                     Hier könnt Ihr Euch mit Eurem Prioritätscode für die 82. Bundesfachschaftenkonferenz Wirtschaftswissenschaften vom 18.01.2026 bis zum 22.02.2026 EOD anmelden.<br /><br />
                     Da die Konferenz vom BMFTR unterstützt wird, benötigen wir von jeder angenommenen Person eine Immatrikulationsbescheinigung für das <b>SommerSemester2026</b>. Bitte benennt die PDF-Datei folgendermaßen: "Stadt_Vorname_Nachname".<br />
-                    Diese könnt Ihr unter folgendem Link hochladen: <a href="https://www.dropbox.com/request/lutk7zJgHnlOtlLFux3y" target="_blank" rel="noopener noreferrer">https://www.dropbox.com/request/lutk7zJgHnlOtlLFux3y</a><br /><br />
+                    Diese könnt Ihr unter folgendem Link hochladen: <a href="https://nc.uni-bremen.de/index.php/s/s93NFj3nWoekWXM" target="_blank" rel="noopener noreferrer">https://nc.uni-bremen.de/index.php/s/s93NFj3nWoekWXM</a><br /><br />
                     Bei Fragen und Anregungen meldet Euch gerne unter dieser E-Mail bei uns: <a href="mailto:guest@stugawiwi.de" target="_blank" rel="noopener noreferrer">guest@stugawiwi.de</a>
                 </Alert>
             </CardBody>
