35319ab862be2d9374041a0491a6c315453565e6
TheTimKoe <jake.myfreshhaltefolie@gmail.com>
2026-07-22 20:59:20 +0200

    Update: Anmeldezeitraum 83. BuFaK auf 01.08.26 - 30.09.26

--- src/components/Widget/UploadInformationCard.js
+++ src/components/Widget/UploadInformationCard.js
@@ -19,7 +19,7 @@ class UploadInformationCard extends Component {
             <CardBody>
                 <Alert color="grey">
                     Herzlich Willkommen auf unserem Konfetti-Anmeldetool! <br /><br />
-                    Hier könnt Ihr Euch mit Eurem Prioritätscode für die 83. Bundesfachschaftenkonferenz Wirtschaftswissenschaften vom 29.10.26 bis zum 01.11.26 EOD anmelden.<br /><br />
+                    Hier könnt Ihr Euch mit Eurem Prioritätscode für die 83. Bundesfachschaftenkonferenz Wirtschaftswissenschaften vom 01.08.26 bis zum 30.09.26 EOD anmelden.<br /><br />
                     Da die Konferenz vom BMFTR unterstützt wird, benötigen wir von jeder angenommenen Person eine Immatrikulationsbescheinigung für das <b>Wintersemester 2026/2027</b>. Bitte benennt die PDF-Datei folgendermaßen: "Stadt_Vorname_Nachname".<br />
                     Diese könnt Ihr unter folgendem Link hochladen: <a href="https://hs-niederrhein.sciebo.de/s/4yEatMZZQGEeTsF" target="_blank" rel="noopener noreferrer">https://hs-niederrhein.sciebo.de/s/4yEatMZZQGEeTsF</a><br /><br />
                     Bei Fragen und Anregungen meldet Euch gerne unter dieser E-Mail bei uns: <a href="mailto:teilnehmendenbetreuung@gladfak.de" target="_blank" rel="noopener noreferrer">teilnehmendenbetreuung@gladfak.de</a>
